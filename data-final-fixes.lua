f321d667fc0385238600aeca61ea1a5e02d02316
Stephen Barnes <stephen6174@gmail.com>
2024-07-07 19:20:31 +0200

    Add new autoplaces, with names and descriptions; controls have no effect yet

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -2,6 +2,19 @@ local newAutoplaces = {}
 
 for _, control in pairs(data.raw["autoplace-control"]) do
 	if control.category == "resource" then
-		log("Resource: " .. control.name)
+		log("Resource: " .. serpent.block(control))
+		table.insert(newAutoplaces, {
+			type = "autoplace-control",
+			name = control.name .. "-FinerWorldOptions-multiplier",
+			--intended_property = control.intended_property,
+			richness = control.richness,
+			order = control.order .. "-FinerWorldOptions",
+			category = "resource",
+			localised_name = {"autoplace-control-names.FinerWorldOptions-extension", control.localised_name or ("autoplace-control-names." .. control.name)},
+			localised_description = {"autoplace-control-description.FinerWorldOptions-extension"},
+			-- TODO separate ones for normal vs squared vs cubed
+		})
 	end
 end
+
+data:extend(newAutoplaces)
